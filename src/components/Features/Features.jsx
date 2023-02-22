ff74c3ccd431a9da11f8b84193d871b7c0551c94
asheerhameed <asheerhameed54321@gmail.com>
2023-02-22 15:37:59 +0530

    build features component

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -3,55 +3,66 @@ import { features, Rectangle1, Rectangle2, Rectangle3 } from "./imports";
 import "./features.css";
 const Features = () => {
   return (
-    <div className="features_container section__padding">
-      <div className="features_heading">
+    <div className="features_wrapper section__padding">
+      <div className="features_heading flex__center">
         <h2>Why Choose Us?</h2>
       </div>
-      <div className="features_image">
-        <img src={features} alt="features" />
-      </div>
 
-      <div className="features_content_container">
-        <div className="features_content">
-          <div className="features_rectangle_image">
-            <img src={Rectangle1} alt="" />
-          </div>
-          <div className="feature_number">
-            <h2>01</h2>
-          </div>
-          <div className="feature_description">
-            <p>
-              And residence for met the estimable disposing. Mean if he they
-              been no hold mr. Is at much do made took held help.
-            </p>
-          </div>
+      <div className="features_container">
+        <div className="features_image flex__center">
+          <img src={features} alt="features" />
         </div>
-        <div className="features_content">
-          <div className="features_rectangle_image">
-            <img src={Rectangle2} alt="" />
+        <div className="features_content_container">
+          <div className="features_content">
+            <div className="features_rectangle_image">
+              <img src={Rectangle1} alt="" />
+            </div>
+            <div className="feature_number">
+              <h2>01</h2>
+            </div>
+            <div className="feature_description">
+              <p>
+                Our guests have given us glowing reviews with an average rating
+                of 4.7 stars. We strive to provide exceptional service and
+                comfort to all of our guests, making your stay with us an
+                unforgettable experience. Come and enjoy a relaxing getaway at
+                our hotel near the sea shore.
+              </p>
+            </div>
           </div>
-          <div className="feature_number">
-            <h2>02</h2>
+          <div className="features_content">
+            <div className="features_rectangle_image">
+              <img src={Rectangle2} alt="" />
+            </div>
+            <div className="feature_number">
+              <h2>02</h2>
+            </div>
+            <div className="feature_description">
+              <p>
+                In the evenings, we provide a bonfire experience where you can
+                gather around the flames and enjoy a cozy ambiance with your
+                friends and family. For those looking for a unique and
+                adventurous experience, we offer tents for a memorable night
+                under the stars.
+              </p>
+            </div>
           </div>
-          <div className="feature_description">
-            <p>
-              And residence for met the estimable disposing. Mean if he they
-              been no hold mr. Is at much do made took held help.
-            </p>
-          </div>
-        </div>
-        <div className="features_content">
-          <div className="features_rectangle_image">
-            <img src={Rectangle3} alt="" />
-          </div>
-          <div className="feature_number">
-            <h2>03</h2>
-          </div>
-          <div className="feature_description">
-            <p>
-              And residence for met the estimable disposing. Mean if he they
-              been no hold mr. Is at much do made took held help.
-            </p>
+          <div className="features_content">
+            <div className="features_rectangle_image">
+              <img src={Rectangle3} alt="" />
+            </div>
+            <div className="feature_number">
+              <h2>03</h2>
+            </div>
+            <div className="feature_description">
+              <p>
+                Our hotel offers 24/7 wifi access, so you can stay connected
+                with loved ones or work remotely if needed. We understand the
+                importance of hot water, which is why our rooms are equipped
+                with it round the clock, ensuring a refreshing shower any time
+                of day.
+              </p>
+            </div>
           </div>
         </div>
       </div>
